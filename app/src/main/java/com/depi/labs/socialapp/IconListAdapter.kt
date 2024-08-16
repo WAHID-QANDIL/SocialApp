b247263330a57896515d3e803a73546d9145c4c7
Wahid Ali <1213089@dtu.edu.eg>
2024-08-16 06:57:20 +0300

    c

--- app/src/main/java/com/depi/labs/socialapp/IconListAdapter.kt
+++ app/src/main/java/com/depi/labs/socialapp/IconListAdapter.kt
@@ -41,37 +41,6 @@ class IconListAdapter(mutableList: MutableList<SocialIcon>, onIconClicklistener:
         val socialIcon = socialIconList[position]
         holder.bind(socialIcon)
 
-        /*
-        //        val context = holder.itemView.context
-        //        when(position)
-        //        {
-        //            0 -> holder.socialIcon = SocialIcon(
-        //                    iconTitle = context.getString(R.string.facebook),
-        //                    imageResourceId = ContextCompat.getDrawable(holder.itemView.context,R.mipmap.facebook)
-        //                )
-        //            1 -> holder.socialIcon = SocialIcon(
-        //                iconTitle = context.getString(R.string.instgram),
-        //                imageResourceId = ContextCompat.getDrawable(holder.itemView.context,R.mipmap.instegram)
-        //            )
-        //            2 -> holder.socialIcon = SocialIcon(
-        //                iconTitle = context.getString(R.string.google),
-        //                imageResourceId = ContextCompat.getDrawable(holder.itemView.context,R.mipmap.google)
-        //            )
-        //            3 -> holder.socialIcon = SocialIcon(
-        //                iconTitle = context.getString(R.string.twitter),
-        //                imageResourceId = ContextCompat.getDrawable(holder.itemView.context,R.mipmap.twitter)
-        //            )
-        //            4 -> holder.socialIcon = SocialIcon(
-        //                iconTitle = context.getString(R.string.tiktok),
-        //                imageResourceId = ContextCompat.getDrawable(holder.itemView.context,R.mipmap.tiktok)
-        //            )
-        //            5 -> holder.socialIcon = SocialIcon(
-        //                iconTitle = context.getString(R.string.youtube),
-        //                imageResourceId = AppCompatResources.getDrawable(holder.itemView.context,R.mipmap.youtube)
-        //            )
-        //        }
-        */
-
     }
 
 
